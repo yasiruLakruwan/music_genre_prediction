d92cc88f000c9f8df7c1bd96a64b7708905a1f31
yasiruLakruwan <yapa1459@gmail.com>
2025-11-13 01:38:36 +0530

    change model

--- AWS_storing/config/data_paths.py
+++ AWS_storing/config/data_paths.py
@@ -1,6 +1,6 @@
 TRAIN_DIR =     r"D:\Web learning\AWS_storing\include\train_data"
 TEST_DATA = r"D:\Web learning\AWS_storing\include\test_data"
-MODEL_SAVE_PATH = "artifacts\model\model.h5"
+MODEL_SAVE_PATH = r"artifacts\model\model.h5"
 
 # Model oparameters  
 BATCH_SIZE = 32     
